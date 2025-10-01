8b95ed2a688eade446335c6a830209356b1984c4
JanSplat2 <jan.splat@outlook.de>
2025-10-01 14:14:37 -0600

    Initial Commit

---
+++ YesChef!/YesChef_App.swift
@@ -0,0 +1,17 @@
+//
+//  YesChef_App.swift
+//  YesChef!
+//
+//  Created by Dittrich, Jan - Student on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct YesChef_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YesChef!Tests/YesChef_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  YesChef_Tests.swift
+//  YesChef!Tests
+//
+//  Created by Dittrich, Jan - Student on 10/1/25.
+//
+
+import Testing
+@testable import YesChef_
+
+struct YesChef_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
